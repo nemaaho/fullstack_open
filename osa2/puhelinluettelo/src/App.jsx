1a40c2faae6433e67fb770bb66e07b143216df34
nemaaho <nemaaho@jyu.fi>
2024-09-26 14:16:53 +0300

    2.7.

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -13,10 +13,14 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
-
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    const nameExists = persons.some(person => person.name === newName)
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const newPerson = { name: newName }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
   }
 
   return (
